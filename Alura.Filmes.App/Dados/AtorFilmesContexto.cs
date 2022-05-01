200f7a8aa28bd41971721cc143ff79b027e4a325
FabTrindade <fabrtrindade@gmail.com>
2022-05-01 20:02:40 -0300

    Adding a EF DB Context and mapping the Actores class according to the existing database.

---
+++ Alura.Filmes.App/Dados/AtorFilmesContexto.cs
@@ -0,0 +1,20 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Dados
+{
+    public class AtorFilmesContexto : DbContext
+    {
+        public DbSet<Ator> Atores { get; set; }
+
+        protected override void OnConfiguring (DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer("Data Source =.; Initial Catalog = AluraFilmes; Integrated Security = true");
+        }
+    }
+}
--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Alura.Filmes.App
+﻿using Alura.Filmes.App.Dados;
+using System;
+
+namespace Alura.Filmes.App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            using (var context = new AtorFilmesContexto())
+            {                
+                foreach(var ator in context.Atores)
+                {
+                    Console.WriteLine(ator);
+                }
+            }
         }
     }
 }
