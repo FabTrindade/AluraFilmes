a8c6daa68b3c9c8f94f221ae5f6c55020cc15429
FabTrindade <fabrtrindade@gmail.com>
2022-05-09 20:51:48 -0300

    Split query alternative for last test.

--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -17,23 +17,37 @@ namespace Alura.Filmes.App
                 context.LogSQLToConsole();
 
                 //Lista os filmes e categorias de um ator.
-                var ator = context.Atores
-                  .Include(a => a.Filmes)
-                  .ThenInclude(fa => fa.Filme)
-                  .ThenInclude(f => f.Categorias)
-                  .ThenInclude(fc => fc.Categoria)
-                  .ToList();
+                
+                var filme = context.Filmes
+                     .Include(c => c.Categorias)
+                     .ThenInclude(fc => fc.Categoria)
+                     .ToList();
 
+                var ator = context.Atores
+                   .Include(a => a.Filmes)
+                   .ThenInclude(fc => fc.Filme);
+
+                
                 foreach (var act in ator)
                 {
-                    Console.WriteLine($"Filme(s) do ator(a) {act.PrimeiroNome} {act.UltimoNome}");
+                    
+                    Console.WriteLine($"Filmes do ator {act.PrimeiroNome} {act.UltimoNome}:");
                     foreach (var film in act.Filmes)
                     {
+                        var q_cat = filme.Where(c => c.Id == film.Filme.Id).FirstOrDefault();
+                                                
                         Console.Write($"\t{film.Filme.Titulo} ({film.Filme.Id})");
-                        foreach (var cat in film.Filme.Categorias)
+                        if (q_cat != null)
                         {
-                            Console.Write(", " + cat.Categoria.Nome);
+                            foreach (var cat in q_cat.Categorias)
+                            {
+                                Console.Write(", " + cat.Categoria.Nome);
+                            }
                             Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine($", sem categoria registrada");
                         }
                     }
                     Console.WriteLine();
